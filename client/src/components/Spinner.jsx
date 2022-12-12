e1f0d980e8044d80b9a7321ba4fee0798ec39592
Phillip48 <phillippereira02@gmail.com>
2022-12-12 16:06:09 -0500

    Added loading spinner

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+function Spinner() {
+  // CSS in index.css
+  return (
+    <div className='loadingSpinnerContainer'>
+      <div className='loadingSpinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
