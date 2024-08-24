86c995d945ff63181e7e10902cb0279edffcbad1
Phillip Pereira <phillippereira02@gmail.com>
2024-08-23 23:03:17 -0400

    Added client router for new model and controller

---
+++ server/routes/api/clientRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const {
+    getClients,
+    getSingleClient,
+    // createClient,
+    updateClient,
+    deleteClient
+} = require('../../controllers/clientController');
+
+// /api/get-link
+
+// Link Routes
+router.route('/client').get(getClients);
+router.route('/clients/:id').get(getSingleClient).delete(deleteClient).put(updateClient);
+
+module.exports = router;
